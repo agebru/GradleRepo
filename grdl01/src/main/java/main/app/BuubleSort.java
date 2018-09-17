b31fc5629b493df25472ffcd11bfe879a416f8f5
Gebru <amanuel.gebru@delta.com>
2018-09-17 09:37:26 -0400

    Added BubbleSort java

---
+++ grdl01/src/main/java/main/app/BuubleSort.java
@@ -0,0 +1,47 @@
+package main.app;
+
+public class BuubleSort {
+
+	public static void main(String[] args) {
+		Integer arr[] = new Integer[] { 32, 16, 18, 40, 36 };
+
+		int k = 3;
+		findkthHighest(arr, 3);
+
+	}
+	
+	
+	
+private static void findkthHighest(Integer[] arr, int i) {
+		
+		for(int k=0;k<arr.length;k++) { //0..4					
+			int temp=0;			
+			for(int j=0;j<arr.length-k-1;j++) {
+				
+				if(arr[k]>arr[j]) {
+					temp=arr[k];
+					arr[k]=arr[k+1];
+					arr[k+1]=temp;
+				}
+				
+				
+			}
+			
+			
+			
+		}
+		
+		
+		int thirdHighest=0;
+		for(int k=0;k<arr.length;k++) {
+			thirdHighest=arr[2];
+			System.out.print(arr[k]+" ");
+		}
+		
+		System.out.println("\n3rd Highest:"+thirdHighest);
+		
+		
+	}
+	
+
+}
